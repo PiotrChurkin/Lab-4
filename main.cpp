b371ec21057da1a2a4224abd45cf838ce044fc20
PiotrChurkin <64709267+PiotrChurkin@users.noreply.github.com>
2024-09-30 07:13:42 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    short x = 1;
+    
+    if(x==1)
+        std::cout << "Positive number";
+    else
+        std::cout << "Negative number";
+
+    return 0;
+}
